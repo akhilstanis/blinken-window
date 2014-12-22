6bde5b1a1a18f1a5f1cf117f34089a692f4e6eeb
Akhil Stanislavose <akhil.stanislavose@gmail.com>
2014-12-22 20:39:23 +0530

    Update pong example to control using web interface

--- examples/pong.rb
+++ examples/pong.rb
@@ -16,15 +16,23 @@ class Pong
   end
 
   def run
-    Thread.new do
-      loop { movebat }
-    end
+    yield self if block_given?
 
     loop do
       break if ball.out?
       moveball
       sleep(speed)
     end
+
+    screen.clear
+  end
+
+  def movebat(dir)
+    previos_pixels = bat.pixels.dup
+    dir == 'left' ? bat.moveleft : bat.moveright
+    previos_pixels.each { |p| screen.set(*(p << 0)) }
+    bat.pixels.each { |p| screen.set(*p) }
+    screen.render
   end
 
   private
@@ -49,18 +57,6 @@ class Pong
     screen.render
   end
 
-  def movebat
-    previos_pixels = bat.pixels.dup
-    case Curses.getch
-    when 'a'
-      bat.moveleft
-    when 's'
-      bat.moveright
-    end
-    previos_pixels.each { |p| screen.set(*(p << 0)) }
-    bat.pixels.each { |p| screen.set(*p) }
-    screen.render
-  end
 
   def screen
     @screen ||= BlinkenWindow::Screen.new(width, height, BlinkenWindow::Shifters::TerminalShifter.new(width, height))
@@ -69,7 +65,7 @@ class Pong
 end
 
 class Bat
-  def initialize(maxx, y, width=2)
+  def initialize(maxx, y, width=1)
     @maxx  = maxx
     @y = y
     @width = width
@@ -84,7 +80,7 @@ class Bat
   end
 
   def touch?(nx, ny)
-    (nx >= lx && nx <= rx) && ny == @y - 1
+    (nx >= lx && nx < rx) && ny == @y - 1
   end
 
   def pixels
@@ -100,6 +96,7 @@ class Bat
   def rx
     lx + @width
   end
+
 end
 
 class Ball
@@ -166,8 +163,109 @@ end
 require 'blinken_window/shifters/terminal_shifter'
 require 'blinken_window/screen'
 
-puts "Instructions: Press 'a' to move left and 's' to move right.\nPress enter to continue..."
-gets
+# Control using input from terminal
+# puts "Instructions: Press 'a' to move left and 's' to move right.\nPress enter to continue..."
+# gets
 
-width, height, speed = 6, 10, 0.2
-Pong.new(width, height, speed).run
+# width, height, speed = 6, 10, 0.5
+# Pong.new(width, height, speed).run do |pong|
+#   Thread.new do
+#     loop do
+#       case Curses.getch
+#       when 'a'
+#         pong.movebat('left')
+#       when 's'
+#         pong.movebat('right')
+#       end
+#     end
+#   end
+# end
+
+# Control using web interface
+require 'sinatra'
+require 'sinatra-websocket'
+
+set :server, 'thin'
+set :bind, '0.0.0.0'
+set :sockets, []
+set :logging, false # Terminal emulator works best when logging if off
+
+class PongManager
+  def initialize
+    width, height, speed = 6, 10, 0.5
+    @pong = Pong.new(width, height, speed)
+
+    Thread.new { @pong.run }
+  end
+
+  def process(msg)
+    case msg
+    when /left/
+      @pong.movebat('left')
+    when /right/
+      @pong.movebat('right')
+    end
+  end
+end
+
+set :pong, nil
+
+
+get '/' do
+  if !request.websocket?
+    settings.pong = PongManager.new
+    erb :index
+  else
+    request.websocket do |ws|
+      ws.onopen do
+        settings.sockets << ws
+      end
+
+      ws.onmessage do |msg|
+        settings.pong.process(msg)
+      end
+
+      ws.onclose do
+        settings.sockets.delete(ws)
+      end
+    end
+  end
+end
+
+__END__
+@@ index
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+    <style>
+      table { width: 100%; height: 100%; }
+      table td { width: 50%; height: 100%; border: #ccc 1px solid; text-align: center; vertical-align: middle; font-size: 500%; }
+      .press { background: #ccc; }
+    </style>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script>
+      $(function(){
+        var ws = new WebSocket('ws://' + window.location.host + window.location.pathname);
+
+        $('.btn').on('touchstart', function(){
+          $(this).addClass('press');
+          ws.send($(this).attr('id'));
+        });
+
+        $('.btn').on('touchend', function(){
+          $(this).removeClass('press');
+        });
+      });
+    </script>
+  </head>
+  <body>
+    <div>
+      <table>
+        <tr>
+          <td id='left' class='btn'>LEFT</td>
+          <td id='right' class='btn'>RIGHT</td>
+        </tr>
+      </table>
+    </div>
+  </body>
+</html>
